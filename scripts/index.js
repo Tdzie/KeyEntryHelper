c19720c25992bf823ec51e87aebd4f4baadcf19d
Tdzie <65301716+Tdzie@users.noreply.github.com>
2023-02-16 11:52:13 -0500

    Lesson one step two adjust

--- scripts/index.js
+++ scripts/index.js
@@ -500,9 +500,10 @@ const lessons = {
 		"lessonOneStepTwo": {
 			"steps":{
 				"stepHeader": ["<h3>Great Work!</h3>",0],
+				"image": ["<img class='imageW50H350' src='images/wildgoodUPC.jpeg'>",0],
 				"stepTwo": ["<p id='p1>>Wild Good Ice Cream is now rung into your order.",1000],
-				"stepThree": ["<p id='p2'>lets try another one!</p>",4000],
-				"image": ["<img class='imageW50H350' src='images/wildgoodUPC.jpeg'>",0]
+				"stepThree": ["<p id='p2'>lets try another one!</p>",4000]
+				
 			}
 		}
 	},
